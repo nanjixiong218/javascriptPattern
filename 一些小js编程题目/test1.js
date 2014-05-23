1c33fde12eeb38f9476b95dd326f8d377ca294e1
xu <695540791@qq.com>
2014-05-23 12:23:21 +0800

    求奇数算法更新

--- 一些小js编程题目/test1.js
+++ 一些小js编程题目/test1.js
@@ -1,19 +1,50 @@
 /**
  *
  * Created by Administrator on 14-5-21.
- * 求0-7能组成的奇数个数，数组不可以重复使用
+ * 求0-7能组成的奇数个数，数字不可以重复使用
  */
-//遍历的方式
+//数学的方法
 function caculate(){
     var i,sum=0,tempSum=0;
-    for(i=2;i<8;i++){
-        tempSum = Anm(i,i)-Anm(i-1,i-1)*2-(Anm(i-1,i-1)-Anm(i-2,i-2))*3;
-        //i代表位数，tempSum为对应位数可以组成多少数字，
-        // 用总的组合数减去以0打头和以0结尾的,在减去2,4,6结尾的组合数
+    for(i=2;i<9;i++){
+        //tempSum = Anm(8,i)-Anm(7,i-1)*5+Anm(6,i-2)*3;
+        //第一种算法，i代表位数，tempSum为对应位数可以组成多少数字，
+        // 用总的组合数减去以0打头和以0,2,4,6结尾的组合数,再加上以0打头，分别以2,4,6结尾的组合数（因为被重复减掉了）
+
+        tempSum = Cnm(4,1)*Cnm(6,1)*Anm(6,i-2);
+        //第二种算法，1,3,5,7中选择一个，在字剩余7个数中去掉0，选择一个，剩下的6个数任意排列
+
         sum += tempSum;
     }
     sum+=4;
     return sum;
+}
+//遍历的方法,好理解但是效率低下，要进行千万次循环判断
+function caculate1(){
+    var i = 0,count=0;
+    for(i=0;i<=76543210;i++){
+        if(isRepeat(i)){
+            continue;
+        }else{
+            if(isOdd(i)){
+                count++;
+            }
+        }
+    }
+    return count;
+}
+//判断是否有重复:有没有很好的算法？
+function isRepeat(num){
+    var sNum = num.toString();
+    var len = sNum.length;
+    var i = 0,j= 0,flag=false;
+    for(i=0;i<len;i++)
+        for(j=i+1;j<len;j++){
+            if(sNum[i]==sNum[j]){
+                flag=true;
+            }
+        }
+    return flag;
 }
 
 //判断是否为奇数
@@ -37,7 +68,8 @@ function Anm (n,m){
 
 
     return function c(n){
-        if(m==0||--m==0){
+        if(m==0) return 1;
+        if(--m==0){
             return n;
         }
         return n*c(n-1);
@@ -48,14 +80,17 @@ function Cnm(n,m){
     return Anm(n,m)/Anm(m,m);
 }
 //test
-try{//在内部trycatch 了，就不需要在外部try了
+/*try{//在内部trycatch 了，就不需要在外部try了
     var a = Anm(1,2);
 }catch(e){
     console.log(e.name+":"+e.message);
 }
 var a = Anm(1,2);
 var b = Cnm(5,2);
+*/
 var c = caculate();
+var c1 = caculate1();
 console.log(c);
+console.log(c1);
 
 
